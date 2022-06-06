64b6e25c0c600c4039e7891e5f913901a4c9d402
solisoares <solisoareswa@gmail.com>
2022-06-06 18:12:01 -0300

    make overfitting in 3objs (yolov4-darknet)

--- nn/3objs/train.sh
+++ nn/3objs/train.sh
@@ -7,8 +7,8 @@ darknet='/home/alexandre/darknet/build_release/darknet'
 project='/home/alexandre/asd-research/nn/3objs'
 data=$project/'data.txt'
 cfg=$project/'yolov4.cfg'
-# weights=$project/'backup/yolov4_best.weights'
+weights=$project/'backup/yolov4_6000.weights'
 
 # Train it
-# $darknet detector train $data $cfg $weights #> $project/'training.log'
-$darknet detector train $data $cfg -map | tee $project/'training.log'
+$darknet detector train $data $cfg $weights -clear #> $project/'training.log'
+# $darknet detector train $data $cfg -map | tee $project/'training.log'
