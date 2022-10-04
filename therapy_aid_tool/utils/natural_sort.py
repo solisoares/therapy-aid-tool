d6287bd8cd8634ed63b5121c2f9c7767640e20a0
solisoares <solisoareswa@gmail.com>
2022-10-04 19:07:09 -0300

    chore: update utils
    update video utils and remove non used ones

--- therapy_aid_tool/utils/natural_sort.py
+++
@@ -1,5 +0,0 @@
-import re
-
-def natural_sort_key(s, _nsre=re.compile('([0-9]+)')):
-    return [int(text) if text.isdigit() else text.lower()
-            for text in _nsre.split(s)]
--- therapy_aid_tool/utils/video.py
+++ therapy_aid_tool/utils/video.py
@@ -18,22 +18,8 @@ def get_video_frames_count(source: str):
     return frames_count
 
 
-def save_video_labels_template(source: str):
-    """Save empty labels for frames without detection
-
-    This is necessary because empty frames do not generate a
-    labels file
-
-    Args:
-        source (str): Video path
-    """
-    frames_count = get_video_frames_count(source)
-    dir_name = os.path.dirname(source)
-    source_name = Path(source).stem
-
-    for frame in range(1, frames_count + 1):
-        label = os.path.join(dir_name, "labels", source_name + f"_{frame}.txt")
-        # If the label do not exist, create it as an empty file
-        if not os.path.isfile(label):
-            with open(label, "w") as f:
-                f.write("")
+def get_video_fps(source: str):
+    cap = cv2.VideoCapture(source)
+    fps = float(cap.get(cv2.CAP_PROP_FPS))
+    cap.release()
+    return fps
